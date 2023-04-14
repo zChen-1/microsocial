796dd191f8bd00b9628ae6276d7bfb8727f080b3
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2023-04-14 15:59:41 -0700

    Simple UI frontend for login page and main page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,50 @@
 <script>
+// @ts-nocheck
 
+    import axios from 'axios';
+    import { onMount } from 'svelte';
+    import { CONTENT_API } from '../utils/api'
+
+    let data = []
+
+    onMount(async () => {
+        try {
+            const response = await axios.get(`${CONTENT_API}/posts`);
+            data = response.data.result
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    });
 </script>
 
 <h1>Microsocial Landing page</h1>
+<div class="container">
+    {#each data as post, i}
+        <div class="post-container">
+            <p>ID: {post.id}</p>
+            <p>{post.username}</p>
+            <p>{post.date}</p>
+            <p>{post.description}</p>
+            <img src={post.image} alt=""/>
+            <p>{post.tags}</p>
+        </div>
+    {/each}
+</div>
 
 <style>
+    .post-container {
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+        padding: 15px;  
+    }
+
+    .container {
+        display: flex;  
+        justify-content: center;
+        gap: 10px;
+    }
     
+    h1 {
+        text-align: center;
+    }
 </style>
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,2 @@
+export const AUTH_API = "http://localhost:8000"
+export const CONTENT_API = "http://localhost:8001/content"
