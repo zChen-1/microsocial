0a3a3674c5e831ee9e89b1dda6bc5f9227d93882
Bruce McKenzie <bjmckenz@gmail.com>
2023-04-25 10:22:11 -0700

    more of initial paginations

--- user/load_test_data.js
+++ user/load_test_data.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { db } = require("./db");
 
-const stmt = db.prepare("INSERT INTO users (name, password) VALUES (?, ?)");
+const stmt = db.prepare("INSERT INTO users (name, password, versionkey) VALUES (?, ?, 1)");
 
 const csv_data = fs.readFileSync("test_users.csv", "utf8");
 csv_data.split("\r\n").forEach((line) => {
@@ -10,11 +10,12 @@ csv_data.split("\r\n").forEach((line) => {
   const name = match[1] + match[2].substring(0, 1);
   console.log({ name, pass });
 
+  var info
   try {
     info = stmt.run([name, pass]);
   } catch (err) {
     if (err.code !== "SQLITE_CONSTRAINT_UNIQUE") {
-      console.log("insert error: ", { err, info, user });
+      console.log("insert error: ", { err, info, user:{name,pass} });
       return;
     }
   }
