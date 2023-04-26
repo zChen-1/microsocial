0765e48698f6b70372edd57fcd85e3e57d6c9086
Bruce McKenzie <bjmckenz@gmail.com>
2023-04-26 15:38:08 -0700

    refactored

--- user/routes/users.js
+++ user/routes/users.js
@@ -8,15 +8,16 @@ var express = require('express')
 var ms = require('ms')
 const { v4: uuidv4 } = require('uuid')
 
-
 var router = express.Router()
 module.exports.router = router
 
-const { uri,fetch } = require('../common')
+const { uri, fetch } = require('../common')
 const { db } = require('../db')
 const { validate } = require('../utils/schema-validation')
+const { regexpCode } = require('ajv/dist/compile/codegen')
 
-function colspec_to_orderby(spec) {
+// convert what they type in the "sort=" to an appropriate column name
+function colspec_to_orderby (spec) {
   // not worried about match failing because we've already validated this
   match = spec.match(/^(\w+)([+-]?)$/)
   column_name = match[1]
@@ -27,10 +28,11 @@ function colspec_to_orderby(spec) {
   return clause
 }
 
-function deleteExpiredResultSets() {
-  expired_results_as_of = new Date;
+function deleteExpiredResultSets () {
+  expired_results_as_of = new Date()
   expired_results_as_of.setSeconds(
-    expired_results_as_of.getSeconds() - ms(process.env.CURSOR_EXPIRATION) / 1000
+    expired_results_as_of.getSeconds() -
+      ms(process.env.CURSOR_EXPIRATION) / 1000
   )
   const expire_stmt = db.prepare(
     'DELETE FROM users_result_sets WHERE set_results_as_of < ?'
@@ -38,11 +40,228 @@ function deleteExpiredResultSets() {
   expire_stmt.run(expired_results_as_of.toISOString())
 }
 
-function deleteResultSetsBySessionId(session_id) {
+function deleteResultSetsBySessionId (session_id) {
   const del_stmt = db.prepare(
     'DELETE FROM users_result_sets WHERE set_session_id = ?'
   )
   del_stmt.run(session_id)
+}
+
+// all the terms we might see in a URL
+const terms = [
+  { term: 'start_at', type: 'int' },
+  { term: 'page_size', type: 'int' },
+  { term: 'id_GE', clause: 'id >= ?', type: 'int' },
+  { term: 'id_GT', clause: 'id > ?', type: 'int' },
+  { term: 'id', clause: 'id = ?', type: 'int' },
+  { term: 'id_EQ', clause: 'id = ?', type: 'int' },
+  { term: 'id_NE', clause: 'id != ?', type: 'int', inverted: true },
+  { term: 'id_LE', clause: 'id <= ?', type: 'int' },
+  { term: 'id_LT', clause: 'id < ?', type: 'int' },
+  { term: 'name_GE', clause: 'name >= ?' },
+  { term: 'name_GT', clause: 'name > ?' },
+  { term: 'name_NE', clause: 'name != ?', inverted: true },
+  { term: 'name', clause: 'name = ?', inverted: false },
+  { term: 'name_EQ', clause: 'name = ?', inverted: false },
+  { term: 'name_LE', clause: 'name <= ?' },
+  { term: 'name_LT', clause: 'name < ?' }
+]
+
+// Which of all the *query* (where-clause) terms are present?
+function any_term_in_request (req, terms) {
+  terms.forEach((t) => {
+    if (t.term in req.query && 'clause' in t) {
+      return true
+    }
+  })
+  return false
+}
+
+function session_id_present (req) {
+  return (
+    'session' in req.query || (req.auth !== undefined && 'session' in req.auth)
+  )
+}
+
+// anything other than start_at/page_size means new query
+function should_create_new_result_set (req) {
+  if (!session_id_present(req)) {
+    return true
+  }
+  return any_term_in_request(req, terms) || 'sort' in req.query
+}
+
+// req.query comes in as strings. but we need them to BE integers to validate
+function to_int (x) {
+  as_int = parseInt(x)
+  if (as_int === NaN) {
+    return x
+  }
+  return as_int
+}
+
+function sort_clause_SQL (req) {
+  if (!('sort' in req.query)) {
+    return 'id'
+  }
+
+  options = req.query.sort.split(',')
+
+  sort_clause = options.map((x) => colspec_to_orderby(x)).join(',')
+
+  return sort_clause
+}
+
+// do we have a session id? if so return it. otherwise create one
+function query_session_id (req) {
+  const authenticated = req.auth !== undefined && 'session' in req.auth
+  session_id = uuidv4()
+  if ('session' in req.query) {
+    session_id = req.query.session
+  }
+  if (authenticated) {
+    session_id = req.auth.session
+  }
+  return session_id
+}
+
+// populate a users_results_set for this session id
+function create_new_result_set (req, session_id) {
+  now = new Date().toISOString()
+
+  where_clauses = []
+  // hardcoding user_id for now
+  where_vals = [1, now, session_id]
+
+  terms.filter( x => ('clause' in x))
+  .forEach((x) => {
+    const { term, clause, inverted } = x
+    if (term in req.query) {
+      if (Array.isArray(req.query[term])) {
+        const conjunction = inverted ? ' AND ' : ' OR '
+        clauses =
+          '(' +
+          Array(req.query[term].length).fill(clause).join(conjunction) +
+          ')'
+        // console.log({term:clauses,vals:req.query[term]})
+        where_clauses.push(clauses)
+        where_vals = where_vals.concat(req.query[term])
+      } else {
+        // console.log({term:clause,val:req.query[term]})
+        where_clauses.push(clause)
+        where_vals.push(req.query[term])
+      }
+    }
+  })
+
+  where_clause = where_clauses.join(' AND ')
+  if (where_clause !== '') {
+    where_clause = ' WHERE ' + where_clause
+  }
+
+  const sort_clause = sort_clause_SQL(req)
+  const base =
+    `INSERT INTO users_result_sets ` +
+    `(set_querying_user_id, set_results_as_of, set_session_id, ` +
+    `set_rownum, ` +
+    `id, name, versionkey) ` +
+    `SELECT ` +
+    `?,?,?,` +
+    `ROW_NUMBER() OVER (ORDER BY ${sort_clause}),` +
+    `id,name,versionkey ` +
+    `FROM ` +
+    `users`
+
+  const result_sql = base + where_clause
+  console.log({ result_sql, where_vals, queryparams: req.query })
+  const result_set_stmt = db.prepare(result_sql)
+
+
+  // creates result rows
+  result_set_stmt.run(where_vals)
+}
+
+// once the result set is populated, we return out of that (not doing the query again)
+function respond_from_existing_result_set (req, res, session_id) {
+  start_at = 'start_at' in req.query ? req.query.start_at : 1
+  page_size =
+    'page_size' in req.query
+      ? req.query.page_size
+      : process.env.DEFAULT_PAGE_SIZE
+
+  // pull from results, not fresh query
+  const get_users_from_set = `SELECT id, name, versionkey, set_rownum FROM users_result_sets WHERE set_session_id = ? ORDER BY set_rownum LIMIT ? OFFSET ?`
+  const get_users_from_set_stmt = db.prepare(get_users_from_set)
+
+  users = get_users_from_set_stmt.all(session_id, page_size, start_at)
+
+  // make sure this is present and accurate -- not gonna trust what they passed in
+  const row_count_map = db.get(
+    'SELECT COUNT(*) AS row_count FROM users_result_sets WHERE set_session_id = ?',
+    [session_id]
+  )
+
+  // post-process users...
+  users.forEach((user) => {
+    user.uri = uri(`/user/${user.id}`)
+
+    // we needed this row to make order by/limit/offset work correctly. but do not show user.
+    delete user.set_rownum
+  })
+
+  const response = {
+    users: users,
+    start_at,
+    page_size,
+    row_count: row_count_map.row_count
+  }
+
+  // we don't add the session id to the results if it is stored in the auth token
+  const authenticated = req.auth !== undefined && 'session' in req.auth
+  if (!authenticated) {
+    response.session = session_id
+  }
+
+  res.json(response)
+}
+
+function adjust_params_for_validation (req) {
+  // have to do this (rewrite strings to ints) to enable validation of integers
+  terms
+    .filter((x) => 'type' in x && x.type === 'int')
+    .map((x) => x.term)
+    .filter((x) => x in req.query)
+    .forEach((x) => {
+      if (Array.isArray(req.query[x])) {
+        req.query[x] = req.query[x].map((y) => to_int(y))
+      } else {
+        req.query[x] = to_int(req.query[x])
+      }
+    })
+}
+
+function are_params_valid (req, res) {
+  // extend filteringspec to disallow arrays for <things>
+  // include session id if it's in the query
+  errors = validate.UserFilteringSpec(req.query, '{query param}')
+  if (errors.length) {
+    res.json(errors)
+    res.statusMessage = 'Invalid data'
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).end()
+    return false
+  }
+
+  if ('sort' in req.query && req.query.sort.length > 0) {
+    options = req.query.sort.split(',')
+    errors = validate.UserSortingSpec(options, '{sort param}')
+    if (errors.length) {
+      res.json(errors)
+      res.statusMessage = 'Invalid data'
+      res.status(StatusCodes.UNPROCESSABLE_ENTITY).end()
+      return false
+    }
+  }
+  return true
 }
 
 /**
@@ -66,171 +285,19 @@ function deleteResultSetsBySessionId(session_id) {
  *         description: Invalid Query
  */
 router.get('/users', async (req, res) => {
-  now = new Date().toISOString()
-
-  console.log({"a":1});
-
-  session_id = uuidv4()
-  if ( 'session' in req.query ) {
-    session_id = req.query.session
-  }
-  if ( req.auth !== undefined && 'session' in req.auth ){
-     session_id = req.auth.session
-  }
-
   // FIXME: arguably we should just do this perodically, but this is easier for now
-  deleteExpiredResultSets();
+  deleteExpiredResultSets()
 
-  to_int = (x) => { 
-    as_int = parseInt(x)
-    if (as_int === NaN) {
-      return x 
-    }
-    return as_int
-  }
-
-  // have to do this (rewrite strings to ints) to enable validation of integers
-  ['start_at','page_size','id_GE','id_LE','id'].forEach( x => {
-    if (x in req.query) {
-      if (Array.isArray(req.query[x])) {
-         req.query[x].map( (y) => { to_int(y) } )
-      } else {
-         req.query[x] = to_int(req.query[x]);
-      }
-    }
-  });
-
-  if ( 'start_at' in req.query ) {
-    start_at = req.query.start_at
-    page_size = 'page_size' in req.query? 
-      req.query.page_size : 
-      process.env.DEFAULT_PAGE_SIZE
-
-    // pull from results, not fresh query
-    const get_users_from_set = `SELECT id, name, versionkey, set_rownum FROM users_result_sets WHERE set_session_id = ? ORDER BY set_rownum LIMIT ? OFFSET ?`
-    const get_users_from_set_stmt = db.prepare(get_users_from_set)
-
-    users = get_users_from_set_stmt.all(session_id, page_size, start_at)
-    users.forEach((user) => {
-      user.uri = uri(`/user/${user.id}`)
-      // we needed this row to make order by/limit/offset work correctly. but do not show user.
-      delete user.set_rownum
-    })
-    res.json({ users: users, start_at, page_size })
-    return;
-  }
-
-  errors = validate.UserFilteringSpec(req.query, '{query param}')
-  if (errors.length) {
-    res.json(errors)
-    res.statusMessage = 'Invalid data'
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).end()
+  adjust_params_for_validation(req)
+  if (!are_params_valid(req, res)) {
     return
   }
-
-  where_clauses = []
-  // hardcoding user_id for now
-  where_vals = [1, now, session_id];
-
-  const terms = [ 
-    {term:'id_GE', clause:'id >= ?'},
-    {term:'id', clause:'id = ?'},
-    {term:'id_EQ', clause:'id = ?'},
-    {term:'id_NE', clause:'id != ?', inverted:true},
-    {term:'id_LE', clause:'id <= ?'},
-    {term:'name_GE', clause:'name >= ?'},
-    {term:'name_NE', clause:'name != ?', inverted:true},
-    {term:'name', clause:'name = ?',inverted:false},
-    {term:'name_EQ', clause:'name = ?',inverted:false},
-    {term:'name_LE', clause:'name <= ?'}
-  ]
-  terms.forEach( x => {
-    const {term,clause,inverted} = x
-    if (term in req.query) {
-      if ( (Array.isArray(req.query[term])) ) {
-        const conjunction = inverted? " AND " : " OR "
-        clauses = "(" + Array(req.query[term].length).fill(clause).join(conjunction) + ")"
-        // console.log({term:clauses,vals:req.query[term]})
-        where_clauses.push(clauses);
-        where_vals = where_vals.concat(req.query[term])
-      } else {
-        // console.log({term:clause,val:req.query[term]})
-        where_clauses.push(clause);
-        where_vals.push(req.query[term]);
-      }
-    }
-  })
-
-  where_clause = where_clauses.join(' AND ')
-  if (where_clause !== '') {
-    where_clause = ' WHERE ' + where_clause
+  session_id = query_session_id(req)
+  if (should_create_new_result_set(req)) {
+    deleteResultSetsBySessionId(session_id)
+    create_new_result_set(req, session_id)
   }
-
-  // console.log({ foo: validate.UserSortOption("foo", "{foo}"),
-  //     id: validate.UserSortOption("id", "{id}"),
-  //     empty: validate.UserSortOption("", "{foo}") })
-
-  // console.log({
-  //   foo: validate.UserSortingSpec(['foo'], '{foo}'),
-  //   id: validate.UserSortingSpec(['id'], '{id}'),
-  //   empty: validate.UserSortingSpec([], '{}'),
-  // })
-
-  sort_clause = 'id'
-  if ('sort' in req.query && req.query.sort.length > 0) {
-    options = req.query.sort.split(',')
-    errors = validate.UserSortingSpec(options, '{sort param}')
-    if (errors.length) {
-      res.json(errors)
-      res.statusMessage = 'Invalid data'
-      res.status(StatusCodes.UNPROCESSABLE_ENTITY).end()
-      return
-    }
-
-    //console.log({ str: req.query.sort, options, sort_clause })
-    sort_clause = options.map((x) => colspec_to_orderby(x)).join(',')
-
-    //console.log({ sort_clause, from: req.query.sort })
-  }
-  deleteResultSetsBySessionId(session_id);
-
-  const base = `INSERT INTO users_result_sets ` +
-          `(set_querying_user_id, set_results_as_of, set_session_id, ` +
-          `set_rownum, ` +
-          `id, name, versionkey) ` +
-        `SELECT ` +
-          `?,?,?,` +
-          `ROW_NUMBER() OVER (ORDER BY ${sort_clause}),` +
-          `id,name,versionkey ` +
-        `FROM ` +
-          `users`
-
-  const result_sql = base + where_clause
-  const result_set_stmt = db.prepare(result_sql)
-
-  console.log({result_sql, where_vals, queryparams:req.query })
-
-  // creates result rows
-  result_set_stmt.run(where_vals)
-
-  const row_count_map = db.get("SELECT COUNT(*) AS row_count FROM users_result_sets WHERE set_session_id = ?",[session_id])
-  const row_count = row_count_map.row_count
-
-  get_vals = [session_id]
-  get_users_from_set = `SELECT id, name, versionkey, set_rownum FROM users_result_sets WHERE set_session_id = ? ORDER BY set_rownum`
-  if ('page_size' in req.query) {
-      get_vals.push( req.query.page_size );
-      get_users_from_set += " LIMIT ?"
-  }
-  const get_users_from_set_stmt = db.prepare(get_users_from_set)
-
-  users = get_users_from_set_stmt.all(get_vals)
-  users.forEach((user) => {
-    user.uri = uri(`/user/${user.id}`)
-    delete user.set_rownum
-  })
-
-  res.json({ session: session_id, row_count, users })
+  respond_from_existing_result_set(req, res, session_id)
 })
 
 /**
