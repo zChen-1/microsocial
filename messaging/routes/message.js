1c264a2ec022e6edb7afac4c51eb057147e85f11
Ricardo D'Angelo Gonzalez <42185864+cardogg@users.noreply.github.com>
2023-05-01 18:15:32 -0700

    Content is now sanitized when posted or updated

--- messaging/routes/message.js
+++ messaging/routes/message.js
@@ -5,6 +5,7 @@ const {
   getStatusCode,
 } = require('http-status-codes');
 var express = require('express');
+var bleach = require('bleach');
 var router = express.Router();
 module.exports.router = router;
 
@@ -161,7 +162,7 @@ router.delete("/message/:message_id", (req, res) => {
 router.put("/message/:message_id", (req, res) => {
   let messageId = req.params.message_id.trim();
   let content = req.body.content;//.trim();
-
+  content = bleach.sanitize(content);
   //TODO: should check for valid author as well
   let getstmt = db.prepare(`SELECT id, thread, author, timestamp, content FROM messages where id = ?`);
   message=getstmt.all([messageId])[0];
