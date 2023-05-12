8e535b5e92b92661370dc7b30ee01636875bddc7
phos@karmotrine <phos@karmotrine>
2023-05-12 02:19:54 -0700

    Cleaned up some comments

--- messaging/swaggerdefs.js
+++ messaging/swaggerdefs.js
@@ -2,7 +2,7 @@
  * @swagger
  * tags: 
  *    - name: Examples
- *      description: Example code. Do not use this as a real service
+ *      description: Example code. Do not use this as a real service.
  *    - name: Users API 
  *    - name: Content API
  *    - name: Auth API
